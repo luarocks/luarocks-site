a4194fd6d80f3fa3ad09b687815b74df6b5f7dcf
leaf corcoran <leafot@gmail.com>
2012-12-04 10:32:19 -0800

    update build script

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ BUILD_DIR="build"
 [ -d "$BUILD_DIR" ] && rm -rf "$BUILD_DIR"
 mkdir "$BUILD_DIR"
 
-files=$(git ls-files | grep -v "sh$\|Tup\|gitignore\|coffee$\|scss$" | sed -e 's/\.moon$/.lua/')
+files=$(git ls-files | grep -v "sh$\|Tup\|gitignore\|coffee$\|scss$\|\/site.moon$" | sed -e 's/\.moon$/.lua/')
+static_files=$(find static/* -type f | grep -v "Tup\|gitignore\|storage\|coffee$\|scss$")
+
 tar -c $files | tar -C "$BUILD_DIR" -x
+tar -c $static_files | tar -C "$BUILD_DIR" -x
 
 # # copy secret
 # tar -c $(find secret/* | grep -v "Tup\|moon$") | tar -C "$BUILD_DIR" -x
-
-# copy assets
-tar -c $(find static/* | grep -v "Tup\|storage\|coffee$\|scss$") | tar -C "$BUILD_DIR" -x
 
 # # strip lua_code_cache
 # cat nginx.conf | grep -v "lua_code_cache" > "$BUILD_DIR/nginx.conf"
