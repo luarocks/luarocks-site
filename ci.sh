81e55924a4a694f0ba734b31dba929fb41abfad1
leaf corcoran <leafot@gmail.com>
2021-01-25 09:23:20 -0800

    install through luarocks build

--- ci.sh
+++ ci.sh
@@ -4,11 +4,7 @@ set -o pipefail
 set -o xtrace
 
 # setup lua
-luarocks --lua-version=5.1 install --local cloud_storage
-luarocks --lua-version=5.1 install --local https://luarocks.org/manifests/leafo/lapis-dev-1.rockspec
-luarocks --lua-version=5.1 install --local https://raw.githubusercontent.com/moteus/ZipWriter/master/rockspecs/zipwriter-0.1.2-1.rockspec
-luarocks --lua-version=5.1 install --local moonrocks
-luarocks --lua-version=5.1 install --local https://raw.githubusercontent.com/leafo/tableshape/master/tableshape-dev-1.rockspec
+luarocks --lua-version=5.1 build --only-deps
 eval $(luarocks --lua-version=5.1 path)
 
 # prepare secrets
