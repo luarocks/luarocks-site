e9a7363e51befc0f4f9155355cf24d7ab8835215
"Matthieu Coudron" <"886074+teto@users.noreply.github.com">
2024-01-22 01:32:02 +0100

    luaPackages: update on 2024-01-22

--- contrib/generated-packages.nix
+++ contrib/generated-packages.nix
@@ -371,6 +371,29 @@ buildLuarocksPackage {
   };
 }) {};
 
+struct = callPackage({ buildLuarocksPackage, fetchurl, lua, luaAtLeast, luaOlder }:
+buildLuarocksPackage {
+  pname = "struct";
+  version = "1.4-1";
+  knownRockspec = (fetchurl {
+    url    = "mirror://luarocks/struct-1.4-1.rockspec";
+    sha256 = "142zbyzqcqmhq10xwnv6h4h5f46gbkbvdfsg9avi8gc3l70y235h";
+  }).outPath;
+  src = fetchurl {
+    url    = "http://www.inf.puc-rio.br/~roberto/struct/struct-0.2.tar.gz";
+    sha256 = "19d72xlflyxs4dpk3d6wafphs82iaql62ngpfflp9b3lkygiy7f7";
+  };
+
+  disabled = (luaOlder "5.1") || (luaAtLeast "5.3");
+  propagatedBuildInputs = [ lua ];
+
+  meta = {
+    homepage = "http://www.inf.puc-rio.br/~roberto/struct/";
+    description = "A library to convert Lua values to and from C structs";
+    license.fullName = "MIT/X";
+  };
+}) {};
+
 tableshape = callPackage({ buildLuarocksPackage, fetchgit, fetchurl, lua, luaOlder }:
 buildLuarocksPackage {
   pname = "tableshape";
